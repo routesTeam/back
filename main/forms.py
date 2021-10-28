e8a6c4c6ad3511b54528470bfacada417f19b822
Владимир Раду <rodny_co@list.ru>
2021-10-28 11:40:33 +0300

    set inital values in form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
-
 class CitiesForm(forms.Form):
-  firstCity  = forms.CharField(label='Введите пункт отправления:', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Москва'}))
-  secondCity = forms.CharField(label='Введите пункт назначения:', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Санкт-Петербург'}))
+  first_city  = forms.CharField(label='Введите пункт отправления:', 
+                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Москва'})
+              )
+  second_city = forms.CharField(label='Введите пункт назначения:', 
+                  widget=forms.TextInput(attrs=
+                    {'class': 'form-control', 'placeholder': 'Санкт-Петербург'}
+                ))
--- main/views.py
+++ main/views.py
@@ -4,5 +4,11 @@ from .forms import CitiesForm
 
 # Create your views here.
 def index(request):
-    form = CitiesForm()
-    return render(request, "index.html", {'form': form})
+    form = CitiesForm(initial={
+      'first_city': request.GET.get('first_city'),
+      'second_city': request.GET.get('second_city')
+    })
+    # form.fields["secondCityValue"].inital = request.GET.get('secondCity')
+    return render(request, "index.html", {
+      'form': form,
+    })
