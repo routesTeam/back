ce864b36ca55b9d61205562404bf79e5032262ce
Владимир Раду <rodny_co@list.ru>
2021-11-10 17:21:21 +0300

    find route and return json

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .forms import CitiesForm
+from .models import Route
 
 # Create your views here.
 def index(request):
@@ -9,6 +10,10 @@ def index(request):
       'second_city': request.GET.get('second_city')
     })
     
+    first_city = request.GET.get('first_city')
+    second_city = request.GET.get('second_city')
+
     return render(request, "index.html", {
       'form': form,
+      'routes': Route.objects.all().filter(first_city=first_city, second_city=second_city)
     })
