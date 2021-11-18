17a2aa8e6f764f3bc2874474a3d1104498e66791
Владимир Раду <rodny_co@list.ru>
2021-11-18 20:43:24 +0300

    priority field in form

--- main/views.py
+++ main/views.py
@@ -15,8 +15,10 @@ def index(request):
     
     first_city = request.GET.get('first_city')
     second_city = request.GET.get('second_city')
+    priority = request.GET.get('priority')
 
     city = City.objects.all()
+    faster = True if priority == 'fast' else False
 
     try:
         res = a_star(first_city, second_city, City.objects.all(), Relation.objects.all(), PropsRelation.objects.all())
@@ -27,5 +29,6 @@ def index(request):
     return render(request, "index.html", {
       'form': form,
       'city': res,
+      'priority': priority,
       'routes': Route.objects.all().filter(first_city=first_city, second_city=second_city)
     })
