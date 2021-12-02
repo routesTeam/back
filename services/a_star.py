95587bf9b6a993b98d516ff40f30f96d2f55b84f
Alexey Alisin <akalisin@mai.education>
2021-12-02 21:40:00 +0300

    add time schedule to a_star

--- services/a_star.py
+++ services/a_star.py
@@ -1,9 +1,45 @@
 import queue
+import datetime
 
 def heuristic(vert_a, vert_b):
     return abs(vert_b.point_x - vert_a.point_x) + abs(vert_b.point_y - vert_a.point_y)
 
-def a_star(start_name, goal_name, cities, relations, props_relation, search_feature):
+
+def get_min_arrival_time(time_list, time_current):
+    time_current_to_date = datetime.datetime.strptime(time_current, '%H:%M')
+    for x_time in time_list:
+        temp_time = datetime.datetime.strptime(x_time, '%H:%M')
+        if (temp_time - time_current_to_date).total_seconds() >= 0:
+            return temp_time, x_time
+    return datetime.datetime.strptime(time_list[0], '%H:%M'), time_list[0]
+
+
+
+def a_star(start_name, goal_name, cities, relations, props_relation, search_feature, departure_time ):
+
+    #just temporary
+    schedule = {
+        1 : ['10:00', '15:00', '20:00'],
+        2 : ['10:00', '15:00', '20:00'],
+        3 : ['10:00', '15:00', '20:00'],
+        4 : ['10:00', '15:00', '20:00'],
+        5 : ['10:00', '15:00', '20:00'],
+        6 : ['10:00', '15:00', '20:00'],
+        7 : ['10:00', '15:00', '20:00'],
+        8 : ['10:00', '15:00', '20:00'],
+        9 : ['10:00', '15:00', '20:00'],
+        10 : ['10:00', '15:00', '20:00'],
+        11 : ['10:00', '15:00', '20:00'],
+        12 : ['10:00', '15:00', '20:00'],
+        13 : ['10:00', '15:00', '20:00'],
+        14 : ['10:00', '15:00', '20:00'],
+        15 : ['10:00', '15:00', '20:00'],
+        16 : ['10:00', '15:00', '20:00'],
+        17 : ['10:00', '15:00', '20:00'],
+        18 : ['10:00', '15:00', '20:00']
+    }
+
+
 
     start_vert = None
     goal_vert = None
@@ -28,6 +64,10 @@ def a_star(start_name, goal_name, cities, relations, props_relation, search_feat
     distance = {}
     reviewed_verts = []
 
+    arrival_departure = {}
+    arrival_departure[start_name] = [departure_time, None]
+
+
     paths[start_name] = None
     path_exists = False;
     distance[start_name] = 0
@@ -35,6 +75,9 @@ def a_star(start_name, goal_name, cities, relations, props_relation, search_feat
     relation_props_in_path = {}
     relation_props_in_path[start_name] = None
 
+    time_waiting = 0
+    temp_departure_time = None
+    str_time = None
 
 
     cities_queue = queue.PriorityQueue()
@@ -47,9 +90,23 @@ def a_star(start_name, goal_name, cities, relations, props_relation, search_feat
         if current_vert.id in reviewed_verts:
             continue
 
+        temp_arrival_time = arrival_departure[current_vert.name][0]
+
+        if current_vert.name != start_name:
+            arrival_departure[paths[current_vert.name]][1] = relation_props_in_path[current_vert.name][3]  # AAAAahahahahahahahahahahahahahahah
+
+        '''print(current_vert.name)
+        print(paths[current_vert.name])
+        print(relation_props_in_path)
+        print(arrival_departure);'''
+
         if current_vert.name == goal_vert.name:
             path_exists = True
             break
+
+
+
+
 
         for x_edge in relations:
             if x_edge.first_city_id == current_vert.id and x_edge.second_city_id not in reviewed_verts:
@@ -65,18 +122,44 @@ def a_star(start_name, goal_name, cities, relations, props_relation, search_feat
                         prop_relation_next = x_prop
                         break
 
-                next_distance = distance[current_vert.name] + dist_to_next
+                #calculation waiting time
+                (temp_departure_time, str_departure_time)  = get_min_arrival_time(schedule[x_edge.id], temp_arrival_time)
+
+                time_waiting = (temp_departure_time - datetime.datetime.strptime(temp_arrival_time, '%H:%M')).total_seconds() / 60
+
+                if time_waiting < 0:
+                    time_waiting = (temp_departure_time +
+                                    datetime.timedelta(days=1)
+                                    - datetime.datetime.strptime(temp_arrival_time, '%H:%M')).total_seconds() / 60
+
+
+
+
+                next_distance = distance[current_vert.name] + dist_to_next + time_waiting if search_feature else 0 #now with waiting time
 
                 if next_vert.name not in distance or next_distance < distance[next_vert.name]:
-
                     distance[next_vert.name] = next_distance
-
                     priority = next_distance + heuristic(goal_vert, next_vert)
 
                     cities_queue.put(((priority, next_vert.id), next_vert))
                     paths[next_vert.name] = current_vert.name
 
-                    relation_props_in_path[next_vert.name] = [prop_relation_next.relation_type, prop_relation_next.time, prop_relation_next.cost]
+
+                    relation_props_in_path[next_vert.name] = [prop_relation_next.relation_type, prop_relation_next.time, prop_relation_next.cost, str_departure_time]
+
+                    print(current_vert.name)
+                    print(next_vert.name)
+                    print(temp_arrival_time)
+                    print(str_departure_time)
+                    print(relation_props_in_path)
+
+                    #arrival_departure[current_vert.name][1] = str_departure_time
+
+                    arrival_departure[next_vert.name] = []
+                    arrival_departure[next_vert.name].append(datetime.datetime.strftime(
+                        temp_departure_time + datetime.timedelta(hours=dist_to_next), '%H:%M'))
+                    arrival_departure[next_vert.name].append(None)
+
 
         reviewed_verts.append(current_vert.id)
 
@@ -96,7 +179,14 @@ def a_star(start_name, goal_name, cities, relations, props_relation, search_feat
 
     final_path[len(final_path)-1] = {'name': final_path[len(final_path)-1], 'props': relation_props_in_path[final_path[len(final_path)-1]]}
 
+    print(final_path)
+    print(arrival_departure)
+
+
     return final_path
+
+
+
 
 
 '''
